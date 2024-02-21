b754ad4e5dbd5a5be00d4119ea30b489c97c11b7
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-02-21 13:19:10 +0530

    Created NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer,
 Subscribe, Footer, CustomerReviews } from "./sections";
-
+import Nav from './components/Nav'
 
 
 const App = () =>(
 
   <main className="relative">
-    {/* <Nav/> */}
+    <Nav/>
     <section className="xl:padding-1 
     -r padding-r padding-b">
-      <Hero/> section
+      <Hero/>
     </section>
     <section className="padding">
       <PopularProducts/> 
