5145a7fb3db88ad3df547031363c00a62df95201
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-02-22 12:23:44 +0530

    img added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Button from '../components/Button'
 import { statistics } from '../constants/index'
-import {bigShoe1} from '../assets/images'
+import {bigShoe} from '../assets/images'
+
+
+
 
 
 
 const Hero = () => {
+
   return (
    <section
    id='home'
-   className='w-full  flex xl:flex-row flex-col justify-center min-h-screen p-2 max-container'>
-    <div className='relative flex-1 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
+   className='w-full  flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
+    <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
     <h1 className='mt-5  text-8xl font-bold max-sm:text-[72px] font-palanquin leading-none'>
-      <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival Arrival</span>
+      <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
       <br/>
       <span className='gradient-txt inline-block mt-3 mb-3 pt-3 pb-3'>Stray</span> Shoes
     </h1>
@@ -20,7 +24,7 @@ const Hero = () => {
     <Button label='Shop now'/>
     
 
-    <div className='flex  items-start flex-wrap w-full mt-10 gap-16  font-montserrat'>
+    <div className='flex justify-start items-start flex-wrap w-full mt-10 gap-16  font-montserrat'>
     {statistics.map((item, index)=>(
       <div key={index}>
        <p className='text-4xl font-bold'>
@@ -36,11 +40,11 @@ const Hero = () => {
     </div>
     </div>
       <div className='relative  flex-1 flex justify-center items-center 
-      xl:min-h-screen max-xl:py-40  bg-red-200   bg-center'>
-        <img src={bigShoe1} 
-        width={300}
-        height={500} 
-        className='object-contain relative z-10'
+      xl:min-h-screen max-xl:py-40  bg-red-200 rounded-full bg-center'>
+        <img src={bigShoe} 
+        width={500}
+        height={700} 
+        className='object-contain relative drop-shadow-2xl  z-10'
         />
       </div>
    </section>
