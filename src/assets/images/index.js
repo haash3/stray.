8d9f34037df9809a0f33f0a4959fa5eb05157dfa
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-02-29 09:50:23 +0530

    Update Featured

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -7,8 +7,8 @@ import headerLogo from './header-logo.svg'
 import offer from './offer.svg'
 
 import featuredMen from './featuredMen.jpg'
-import featuredWomen from './featuredWomen.jpg'
-import featuredUnisex from './featuredUnisex.png'
+import featuredWomen from './featuredWomen.jpeg'
+import featuredUnisex from './featuredUnisex.jpg'
 
 
 
