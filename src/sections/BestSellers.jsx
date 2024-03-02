3a565c3cbded75835dbf6addd5b6cfcec1266956
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-03-02 17:25:06 +0530

    Carousel created

--- src/sections/BestSellers.jsx
+++ src/sections/BestSellers.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { bestSellers } from '../constants/products'
 
 const BestSellers = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  };
   return (
-    
     <section>
-       <h2 className="text-[30px] font-semibold
-         tracking-tighter font-montserrat">
-          Best Sellers
-        </h2>
-      <div className='mt-5'>
-        {bestSellers.map((product)=>(
-          <div key={product.name}>
-             <img src={product.imgURL} width={350}
-             
-             />
-             <p>{product.name}</p>
-
-             <p>{product.price}</p>
+      <h2 className="text-3xl font-semibold tracking-tighter font-montserrat mb-5">
+        Best Sellers
+      </h2>
+      <Slider {...settings}>
+        {bestSellers.map((product) => (
+          <div key={product.name} className="m-2 p-3 zoom-out">
+            <img
+              src={product.imgURL}
+              alt={product.name}
+              className="w-full object-contain"
+            />
+            <div className="mt-2">
+              <p className="text-lg font-semibold">{product.name}</p>
+              <p className="text-gray-600">{product.price}</p>
+            </div>
           </div>
         ))}
-      </div>
+      </Slider>
     </section>
-  )
-}
-
+  );
+};
 export default BestSellers
